95b8a180666558e454314fe9c0e0eedf808b684b
Youssef <Y.Ehab2289@nu.edu.eg>
2025-03-19 17:10:39 +0200

    refactor: change reading the df as excel not csv

--- load.py
+++ load.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 2:
 file_path = sys.argv[1]
 
 try:
-    df = pd.read_csv(file_path, encoding="ISO-8859-1")
+    df = pd.read_excel(file_path)
     print("Dataset loaded successfully!")
     print(df.head())
 except Exception as e:
